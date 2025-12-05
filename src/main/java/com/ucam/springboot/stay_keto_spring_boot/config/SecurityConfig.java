67eff5999dd8d75e5464cd19f315132e0fd905fd
Eliana <elianadtorrest@gmail.com>
2025-12-05 13:01:44 +0100

    Application properties suitable for Render Free

--- src/main/java/com/ucam/springboot/stay_keto_spring_boot/config/SecurityConfig.java
+++ src/main/java/com/ucam/springboot/stay_keto_spring_boot/config/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig  {
                         // PÚBLIC
                         .requestMatchers("/auth/login", "/auth/register").permitAll()
                         .requestMatchers("/images/**", "/food/**", "/uploads/**") .permitAll()
-                        .requestMatchers("/api/searchByName").permitAll()
+                        .requestMatchers("/food/searchByName").permitAll()
 
                         // PRIVATE
                         .requestMatchers("/api/habit/**").authenticated()
